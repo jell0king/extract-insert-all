a4574acb8701c8c5045227bbde4bc8ac3b21dff6
jell0king <167980358+jell0king@users.noreply.github.com>
2024-09-25 11:21:20 +0500

    out.py

--- out.py
+++ out.py
@@ -1,20 +1,16 @@
 import os
 import shutil
 
-# Define the directory where all your folders are located
-root_directory = "Y:\Шушарин И.А\ЗК\РТЗК54"
+root_directory = "directory\path\folder"
 
-# Walk through all subdirectories and files in the root directory
 for root, dirs, files in os.walk(root_directory):
     for file in files:
-        # Construct the full file path
+
         file_path = os.path.join(root, file)
 
-        # Move the file to the parent folder (root)
         parent_folder = os.path.dirname(root)
         shutil.move(file_path, root_directory)  # Moves the file to the root directory
 
-# Remove any empty folders
 for root, dirs, files in os.walk(root_directory, topdown=False):
     for dir_name in dirs:
         dir_path = os.path.join(root, dir_name)
